b8887d1d343d025711cb79778770fae3074ddc19
akihiro <gmakihiro@gmail.com>
2025-07-13 23:40:49 +0900

    a

--- src/main/java/com/jp/login/constants/ApplicationConstants.java
+++ src/main/java/com/jp/login/constants/ApplicationConstants.java
@@ -1,35 +1,29 @@
 package com.jp.login.constants;
 
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ApplicationConstants {
     public static final String SLASH = "/";
     public static final String REDIRECT = "redirect:";
 
-    @NoArgsConstructor(access = AccessLevel.PRIVATE)
     public static final class RegisterConstants {
         public static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
         public static final int PASSWORD_LENGTH = 15;
     }
 
-    @NoArgsConstructor(access = AccessLevel.PRIVATE)
     public static final class ApplicationBase {
         public static final String ROOT = "/";
         public static final String USERNAME = "userName";
         public static final String userNameCheck = "userNameCheck";
-        public static final String login = "login";
+        public static final String LOGIN = "login";
         public static final String register = "register";
     }
 
     public static final class ApplicationToUrl {
-        public static final String login_to_register = ApplicationBase.login + SLASH + ApplicationBase.register;
-        public static final String login_to_userName = ApplicationBase.login + SLASH + ApplicationBase.USERNAME;
+        public static final String login_to_register = ApplicationBase.LOGIN + SLASH + ApplicationBase.register;
+        public static final String login_to_userName = ApplicationBase.LOGIN + SLASH + ApplicationBase.USERNAME;
     }
 
     public static final class ApplicationFromUrl {
-        public static final String from_login = ApplicationBase.login + SLASH + ApplicationBase.login;
+        public static final String from_login = ApplicationBase.LOGIN + SLASH + ApplicationBase.LOGIN;
     }
 
         public static final class ApplicationRedirectUrl {
