f394931c9e4998952fc1f5d46c9d9ee75c2c95b0
akihiro <gmakihiro@gmail.com>
2025-08-03 23:13:11 +0900

    a

--- src/main/java/com/jp/login/controller/RootController.java
+++ src/main/java/com/jp/login/controller/RootController.java
@@ -1,11 +1,8 @@
 package com.jp.login.controller;
 
-import static com.jp.login.constants.ApplicationConstants.ApplicationBase.ROOT;
-import static com.jp.login.constants.ApplicationConstants.ApplicationBase.USERNAME;
-import static com.jp.login.constants.ApplicationConstants.ApplicationBase.register;
-import static com.jp.login.constants.ApplicationConstants.ApplicationRedirectUrl.from_userName_redirect;
-import static com.jp.login.constants.ApplicationConstants.ApplicationToUrl.login_to_register;
-import static com.jp.login.constants.ApplicationConstants.ApplicationToUrl.login_to_userName;
+import static com.jp.login.constants.ApplicationConstants.ApplicationBase.*;
+import static com.jp.login.constants.ApplicationConstants.ApplicationRedirectUrl.*;
+import static com.jp.login.constants.ApplicationConstants.ApplicationToUrl.*;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
@@ -52,7 +49,7 @@ class Dev1Controller {
 
     @PostMapping("/content")
     public String selectItem(
-            @RequestParam(name = "screenName", defaultValue = "未選択") String screenName,
+            @RequestParam(defaultValue = "未選択") String screenName,
             Model model) {
         model.addAttribute("errorMessage", null);
         model.addAttribute("screens", contentItemMapper.findAll());
